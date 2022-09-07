763fe88badad741f5f4bf16142187b9a50c9abdb
EmanRiziq <emanriziq@gmail.com>
2022-09-07 14:12:10 +0300

    change port

--- middlewares/validate-number.js
+++ middlewares/validate-number.js
@@ -1,12 +1,13 @@
 'use strict'
-module.exports = (arg) => {
+module.exports = () => {
     return (req, res, next) => {
-        if (!isNaN(arg)) {
+        console.log(req.query.query)
+        if (!isNaN(parseInt(req.query.query))) {
             // req.squarenum = Math.pow(parseInt(arg),2)
-            req.squarenum =  (Math.pow(parseInt(arg),2)).toString();
+            req.squarenum =  (Math.pow(parseInt(req.query.query),2)).toString();
             next();
         } else {
-            next(`${arg} is not a number`);
+            next(`${req.query.query} is not a number`);
         }
     }
 }
--- server.js
+++ server.js
@@ -6,29 +6,25 @@ const cors = require('cors');
 const validateNum = require('./middlewares/validate-number');
 const errorHandler = require('./error-handlers/500');
 
-
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  console.log('inside the get method')
-  res.status(200).send('Hello World');
+    res.status(200).send('Hello World');
 })
 
-
-
-app.get('/square', validateNum(9),(req, res) => {
+app.get('/square', validateNum(), (req, res) => {
     const data = req.squarenum;
-        res.status( 200 ).send( `the square of 9 is ${data}` );
-  })
+    res.status(200).send(`the square of ${req.query.query} is ${data}`);
+})
 
-  app.use(errorHandler);
+app.use(errorHandler);
 
 function start(port) {
-  app.listen(3000, () => console.log(`server up a wake`));
+    app.listen(port || 3000, () => console.log(`server up a wake`));
 }
 
 module.exports = {
-  app: app,
-  start: start
+    app: app,
+    start: start
 }
